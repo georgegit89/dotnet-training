f7f50ded25bcc3774cab7af6adec9de653acb124
George James <GeorgeJa@nedbank.co.za>
2025-09-08 10:31:46 +0530

    latest changes

---
+++ Solutions/Day9/CollectionSolution/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace entities;
+
+using System.Collections.Generic;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+}
