c6022e9e92cb06e870bb5659efa1602042929e70
George James <GeorgeJa@nedbank.co.za>
2025-09-08 09:43:26 +0530

    fix the changes

---
+++ Solutions/Day6/ECommerceSolution/TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using CatalogLib;
+
+
+Console.WriteLine("Hello, World!");
+
+Product latestOnDemandingProduct=new Product();
+latestOnDemandingProduct.Name= "Macbook Pro";
+latestOnDemandingProduct.Price = 1200.00m;
+
+
+
+//Property Initialization Syntax
+var product = new Product
+{
+    Id = 1,
+    Name = "Sample Product",
+    Price = 569.99m,
+    Description = "This is a sample product."
+};
+
+Console.WriteLine(product);
