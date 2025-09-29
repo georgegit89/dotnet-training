437b6e8d953d032d13ac4ae90c5909491c4275b2
George James <GeorgeJa@nedbank.co.za>
2025-09-29 11:18:09 +0530

    latest changes

--- Solutions/Assignment/CapstoneSolution/UsersWebAPI/Program.cs
+++ Solutions/Assignment/CapstoneSolution/UsersWebAPI/Program.cs
@@ -4,7 +4,14 @@ using Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDBSettings"));
 
+builder.Services.AddSingleton<IMongoDatabase>(options =>
+{
+    var settings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDbSettings>();
+    var client = new MongoClient(settings.ConnectionString);
+    return client.GetDatabase(settings.DatabaseName);
+});
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
